8e2700da1da731a54d9c274474d1fa54f5fdc276
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-07-13 10:52:13 +0530

    Time: 37 ms (15.78%), Space: 25.8 MB (82.05%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hashset = set()
+
+        for n in nums:
+            hashset.add(n)
+
+        return len(nums) != len(hashset)
+        
