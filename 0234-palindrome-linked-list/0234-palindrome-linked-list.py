616d1b50ad0b92408d4eec54af0d005e049224c3
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-07-31 01:08:00 +0530

    Time: 104 ms (99.09%), Space: 66.5 MB (60.3%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,12 +5,30 @@
 #         self.next = next
 class Solution(object):
     def isPalindrome(self, head):
-        pal_str = ""
-
-        while head:
-            pal_str += str(head.val)
-            head = head.next
-
-        return pal_str == pal_str[::-1]
-
         
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev = None
+        curr = slow 
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        first = head
+        second = prev
+
+        while second:
+            if first.val != second.val:
+                return False
+            first= first.next
+            second=second.next
+
+        return True
