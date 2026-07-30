cd1dc8b649f13bd4c9ba95778a7362214d432886
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-07-30 14:35:08 +0530

    Time: 0 ms (100%), Space: 12.5 MB (26.88%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -5,16 +5,16 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         if numRows == 0:
-            return
+            return 
 
-        res = [[1]]
+        res=[[1]]
         for i in range(1,numRows):
             prev = res[-1]
             row = [1]
 
             for j in range(1,i):
-                row.append(prev[j-1]+prev[j])
-
+                row.append(prev[j-1] + prev[j])
+            
             row.append(1)
             res.append(row)
 
