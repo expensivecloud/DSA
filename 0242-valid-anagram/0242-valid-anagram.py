d74b5fd96b7c66228486b5ba477b63ff00b876db
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-07-31 17:00:52 +0530

    Time: 23 ms (50.19%), Space: 14.4 MB (8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,5 +5,20 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return sorted(s)== sorted(t)
+        n = len(s)
+
+        if n != len(t):
+            return False
+
+        countT, countS = {},{}
+
+        for i in range(n):
+            countS[s[i]] = 1 + countS.get(s[i],0)
+            countT[t[i]] = 1 + countT.get(t[i],0)
+
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+
+        return True
         
