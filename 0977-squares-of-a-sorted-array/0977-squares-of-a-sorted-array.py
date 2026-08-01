cca44a7165248a0b8018cc539ebcbf1005126bbd
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-08-01 15:49:24 +0530

    Time: 15 ms (42.75%), Space: 14.1 MB (72.47%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,9 +1,29 @@
 class Solution(object):
     def sortedSquares(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+
+        n = len(nums)
+        res = [0] * n
+
+        left = 0
+        right = n - 1
+        pos = n - 1
+
+        while left <= right:
+
+            l2 = nums[left] ** 2
+            r2 = nums[right] ** 2
+
+            if l2 > r2:
+                res[pos] = l2
+                left += 1
+            else:
+                res[pos] = r2
+                right -= 1
+
+            pos -= 1
+
+        return res
+        
         for i in range(len(nums)):
             nums[i] = nums[i] * nums[i]
 
