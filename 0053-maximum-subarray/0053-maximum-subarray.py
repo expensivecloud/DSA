432091d88f5910fb4fbe189b83249ac793a185ee
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-08-07 22:22:17 +0530

    Time: 97 ms (40.76%), Space: 20.9 MB (99.56%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -7,8 +7,8 @@ class Solution(object):
         current_sum = nums[0]
         max_sum = nums[0]
 
-        for x in nums[1:]:
-            current_sum = max(x, current_sum + x)
-            max_sum = max(max_sum, current_sum)
+        for num in nums[1:]:
+            current_sum = max(num,current_sum+num)
+            max_sum = max(max_sum,current_sum)
 
         return max_sum
