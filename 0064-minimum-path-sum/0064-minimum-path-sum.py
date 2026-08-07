2cf6174bb87bbfa78dcc6522ca1bc341af484074
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-08-08 01:22:06 +0530

    Time: 11 ms (89.32%), Space: 14.2 MB (29.88%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+
+        n = len(grid)
+        m = len(grid[0])
+
+        dp = [[0] * m for _ in range(n)]
+
+        dp[n - 1][m - 1] = grid[n - 1][m - 1]
+
+        for j in range(m - 2, -1, -1):
+            dp[n - 1][j] = grid[n - 1][j] + dp[n - 1][j + 1]
+
+        for i in range(n - 2, -1, -1):
+            dp[i][m - 1] = grid[i][m - 1] + dp[i + 1][m - 1]
+
+        for i in range(n - 2, -1, -1):
+            for j in range(m - 2, -1, -1):
+                dp[i][j] = grid[i][j] + min(dp[i + 1][j], dp[i][j + 1])
+
+        return dp[0][0]
