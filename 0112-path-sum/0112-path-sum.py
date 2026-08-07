21ed5e3f90a75607d9508c5e0f6370eb65f573fb
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-08-08 01:10:58 +0530

    Time: 4 ms (29.04%), Space: 14.1 MB (66.06%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: Optional[TreeNode]
+        :type targetSum: int
+        :rtype: bool
+        """
+        def dfs(node,currSum):
+
+            if not node:
+                return False
+
+            currSum += node.val
+
+            if not node.left and not node.right:
+                return currSum == targetSum
+
+            return dfs(node.left,currSum) or dfs(node.right, currSum)
+
+
+        return dfs(root,0) 
