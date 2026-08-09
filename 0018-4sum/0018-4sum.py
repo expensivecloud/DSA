cb11ae4f648e51d9e43d356487b7439b84ac8dcd
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-08-09 16:22:20 +0530

    Time: 534 ms (49.86%), Space: 12.4 MB (29.55%) - LeetHub

---
+++ 0018-4sum/0018-4sum.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        n = len(nums)
+        res = []
+
+        for i in range(n):
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i+1,n):
+
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                    
+                left = j+1
+                right = n-1
+
+                while left<right:
+                    sumo = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if target == sumo:
+                        res.append([nums[i],nums[j],nums[left],nums[right]])
+                        left += 1
+                        right -= 1
+
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+
+                    elif target > sumo:
+                        left += 1
+                    else:
+                        right -= 1
+
+        return res
+        
