319bba3f92ccc740c6ea060549ee703b3d5df03a
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-08-11 15:32:47 +0530

    Time: 0 ms (100%), Space: 12.3 MB (57.7%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [-1] * n
+        dp = [-1] * (n+1)
 
-        dp[0] = 0
+        dp[0] = 1
         dp[1] = 1
 
-        for i in range(2,n):
-            dp[i] = max(dp[i-1],dp[i-2])
+        for i in range(2,n+1):
+            dp[i] = dp[i-1]+dp[i-2]
 
-        return dp[n-1]
+        return dp[n]
