6424f35912d5f269cd77c58cc0d9a26770879a4d
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-08-12 21:47:30 +0530

    Time: 16 ms (66.62%), Space: 12.5 MB (16.5%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [-1] * (n+1)
+
+        def f(n,dp):
+            if n == 0:
+                return 0
+            if n == 1:
+                return 1
+
+            if dp[n] != -1:
+                return dp[n]
+
+            dp[n] = f(n-1,dp) + f(n-2,dp)
+
+            return dp[n]
+
+        return f(n,dp)
