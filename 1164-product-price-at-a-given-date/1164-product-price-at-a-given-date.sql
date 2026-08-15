ca926ba04b91c96f315b03cee52f0e2d7921beaa
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-08-15 23:28:08 +0530

    Time: 1523 ms (5.66%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,19 @@
+SELECT p.product_id, p.new_price AS price
+FROM Products p
+WHERE p.change_date = (
+    SELECT MAX(p2.change_date)
+    FROM Products p2
+    WHERE p2.product_id = p.product_id
+      AND p2.change_date <= '2019-08-16'
+)
+
+UNION
+
+SELECT product_id, 10 AS price
+FROM Products
+WHERE product_id NOT IN (
+    SELECT product_id
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+)
+GROUP BY product_id;
