467d50888739449105710eb6df5f2489738edf18
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-08-16 23:12:13 +0530

    Time: 11 ms (14.43%), Space: 12.3 MB (59.27%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,40 +1,44 @@
 from collections import deque
+
 class Solution(object):
     def orangesRotting(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        
-        dr = [(1,0),(-1,0),(0,1),(0,-1)]
+        n = len(grid)
+        m = len(grid[0])
 
-        q = deque()
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+
         fresh = 0
+        q = deque()
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-
+        # Add all rotten oranges
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j] == 1:
                     fresh += 1
+                elif grid[i][j] == 2:
+                    q.append((i, j))
 
-                if grid[i][j] == 2:
-                    q.append([i,j,0])
+        mins = 0
 
-        ans = 0
+        while q and fresh > 0:
 
-        while q:
-            x,y,t = q.popleft()
-            ans = max(ans,t)
+            # Process all oranges at the current minute
+            for _ in range(len(q)):
 
-            for dx,dy in dr:
-                nx = x + dx
-                ny = y + dy
+                x, y = q.popleft()
 
-                if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == 1:
-                    grid[nx][ny] = 2
-                    fresh -= 1
-                    q.append([nx,ny,t+1])
+                for dx, dy in dirs:
+                    nx = x + dx
+                    ny = y + dy
 
+                    if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == 1:
+                        grid[nx][ny] = 2
+                        fresh -= 1
+                        q.append((nx, ny))
 
-        return ans if fresh == 0 else -1
+            mins += 1
 
+        if fresh > 0:
+            return -1
+
+        return mins
