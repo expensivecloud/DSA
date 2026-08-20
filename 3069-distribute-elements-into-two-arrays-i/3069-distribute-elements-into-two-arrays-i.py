00b845a2fad71ea5917f6e70ccb6854b47b5d93e
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-08-21 00:17:36 +0530

    Time: 0 ms (100%), Space: 12.4 MB (48.39%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.py
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.py
@@ -1,22 +1,13 @@
-class Solution(object):
+class Solution:
     def resultArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
         arr1 = [nums[0]]
         arr2 = [nums[1]]
 
-        last_1 = nums[0]
-        last_2 = nums[1]
-
-        for i in range(2,len(nums)):
-            if last_1 > last_2:
+        for i in range(2, len(nums)):
+            if arr1[-1] > arr2[-1]:
                 arr1.append(nums[i])
-                last_1 = nums[i]
             else:
                 arr2.append(nums[i])
-                last_2 = nums[i]
 
-        return arr1+arr2
-
+        return arr1 + arr2
+        
