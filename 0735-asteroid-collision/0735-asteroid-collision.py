68f8d55371802fc456e2b574c7bfedd2484e086c
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-08-22 00:53:15 +0530

    Time: 7 ms (85.32%), Space: 13.1 MB (77.11%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+
+        stack = []
+
+        for a in asteroids:
+            while stack and a < 0 and stack[-1] > 0:
+
+                if stack[-1] < -a:
+                    stack.pop()
+
+                elif stack[-1] == -a:
+                    stack.pop()
+                    break
+
+                else:
+                    break
+
+            else:
+                stack.append(a)
+
+        return stack
