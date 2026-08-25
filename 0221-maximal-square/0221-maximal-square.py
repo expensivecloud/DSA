ed741cf8682416bf64349796b25e3ff92c0a9ae1
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-08-26 01:44:35 +0530

    Time: 167 ms (59.11%), Space: 25.9 MB (71.02%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+
+        n = len(matrix)
+        m = len(matrix[0])
+
+        dp = [[0] * m for _ in range(n)]
+
+        ans = 0
+
+        for i in range(n):
+            for j in range(m):
+
+                if matrix[i][j] == "1":
+
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = 1 + min(
+                            dp[i-1][j],
+                            dp[i][j-1],
+                            dp[i-1][j-1]
+                        )
+
+                    ans = max(ans, dp[i][j])
+
+        return ans * ans
