008b13a08a2682b69dda39d9db2760fcf50a41e8
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-09-01 19:56:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = list(set(nums))
+
+        if len(nums) < 3:
+            return max(nums)
+
+        nums.sort(reverse=True)
+
+        return nums[2]
