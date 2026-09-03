25e50cc901f16d7138ca3f42d5c28d16fc08e78b
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-09-03 19:13:01 +0530

    Time: 0 ms (100%), Space: 12.4 MB (96.65%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        running_sum = [0] * n
+        running_sum[0] = nums[0]
+
+        for i in range(1,n):
+            running_sum[i] = running_sum[i-1] + nums[i]
+
+        return running_sum
