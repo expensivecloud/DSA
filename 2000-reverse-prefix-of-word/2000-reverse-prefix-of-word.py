e04f317bd8dba6845a4c6f797aa32cf8d89288f0
expensivecloud <150516216+expensivecloud@users.noreply.github.com>
2026-09-04 00:51:40 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.09%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reversePrefix(self, word, ch):
+        """
+        :type word: str
+        :type ch: str
+        :rtype: str
+        """
+        for i in range(len(word)):
+            if word[i] == ch:
+                return word[:i+1][::-1] + word[i+1:]
+
+        return word
